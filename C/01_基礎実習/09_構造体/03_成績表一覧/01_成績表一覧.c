0c2b881b6d5f0c8a329fcb22d7c101471b891f77
Ayu Iha <17oa03@gmail.com>
2017-07-04 23:22:47 +0900

    ソースコードの修正を行いました。

--- C/01_基礎実習/09_構造体/03_成績表一覧/01_成績表一覧.c
+++ C/01_基礎実習/09_構造体/03_成績表一覧/01_成績表一覧.c
@@ -5,54 +5,59 @@
 /*【プログラム】*/
 #include <stdio.h>
 
+/* 構造体配列の要素数 */
 #define ELEMENT 10
+
+/* 構造体の宣言 */
+struct record {
+	int    no;	 /* 番号 */
+	char   name[11]; /* 名前 */
+	int    english;  /* 英語の点数 */
+	int    math;     /* 数学の点数 */
+	int    language; /* 国語の点数 */
+	int    total;    /* 合計点 */
+	double average;  /* 平均点 */
+	int    rank;     /* 順位 */
+};
 
 int main(void)
 {
-	struct record {
-		int    no;
-		char   name[11];
-		int english;
-		int math;
-		int language;
-		int total;
-		double average;
-		int rank;
-	};
-
+	/* 構造体変数の定義 */
 	struct record data[ELEMENT];
 
-	int i, j;
-	
-	int temp;
+	/* カウンタ変数 */
+	int i, j, k;
 
-	for (i = 0; i < ELEMENT; i++) {
+	/* データの読み込み終了フラグ */
+	int end_flag = 0;
+
+	/* 入力、合計、平均、順位の処理と読み込みのカウント */
+	for (k = 0; end_flag != EOF; k++) {
+
 		scanf("%d %s %d %d %d",
-			&data[i].no, data[i].name, &data[i].english, &data[i].math, &data[i].language);
+			&data[k].no, data[k].name, &data[k].english, &data[k].math, &data[k].language);
+
+		data[k].total = data[k].english + data[k].math + data[k].language;
+		data[k].average = (double) data[k].total / 3;
+		data[k].rank = 1;
 	}
 
-	for (i = 0; i < ELEMENT; i++){
-		data[i].total = data[i].english + data[i].math + data[i].language;
-		data[i].average = (double)data[i].total / 3;
-		data[i].rank = i + 1;
-	}
+	/* 順位を決める処理 */
+	for (i = 0; i < k - 1; i++) {
+		for (j = i + 1; j < k; j++) {
 
-	for (i = 0; i < ELEMENT - 1; i++) {
-		for (j = i + 1; j < ELEMENT; j++) {
+			if (data[i].total < data[j].total) {
 
-			if (data[i].total > data[j].total) {
-
-				temp = data[j].rank;
-				data[j].rank = data[i].rank;
-				data[i].rank = temp;
+				data[i].rank++;
 			}
 		}
 	}
 
+	/* 出力 */
 	printf(" NO 氏    名   英語 数学 国語 合計  平均 順位\n");
 	printf("--- ---------- ---- ---- ---- ---- ----- ----\n");
-	
-	for (i = 0; i < ELEMENT; i++) {
+
+	for (i = 0; i < k; i++) {
 
 		printf("%3d ", data[i].no);
 		printf("%-11s", data[i].name);
@@ -60,8 +65,8 @@ int main(void)
 		printf("%4d ", data[i].math);
 		printf("%4d ", data[i].language);
 		printf("%4d ", data[i].total);
-		printf("%4.1d ", data[i].average);
-		printf("%4%", data[i].rank);
+		printf("%4.1lf ", data[i].average);
+		printf("%4d", data[i].rank);
 	}
 
 	return 0;

579a2f8d9149dbf342143e22da3d9f6c506ceb42
Ayu Iha <17oa03@gmail.com>
2017-07-09 20:37:13 +0900

    python3に「02_bool関数().txt」を追加、さらに「C#」フォルダを追加、試験的にスクリプトファイルを追加しました。

---
+++ C#/01_基礎実習/01_スクリプトの基礎/01_コンソールビュー表示/test.cs
@@ -0,0 +1,37 @@
+﻿/*【プログラム概要】
+ * コンソールビューに何か表示させる。
+*/
+
+// 【プログラム】
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // コンソールビューに表示する
+        Debug.Log("Hello Work!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+/*【考察】
+ * スクリプトを作成し、
+ * ヒエラルキービューに登録された
+ * ゲームオブジェクトにアタッチする際に
+ * "Can't add script"エラーが発生。
+ * 
+ * 原因は、
+ * ファイル名と
+ * スクリプトのクラス名の相違、
+ * 作成時のファイル名から
+ * 名前を変更した場合、
+ * クラス名も変更、
+ * 確認する必要がある。
+ */

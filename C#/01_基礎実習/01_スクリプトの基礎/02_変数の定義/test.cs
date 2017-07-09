0dd900b80a0f1908005cd4cae9a5d18d597c7212
Ayu Iha <17oa03@gmail.com>
2017-07-09 20:59:05 +0900

    「02_変数の定義」を追加しました。

---
+++ C#/01_基礎実習/01_スクリプトの基礎/02_変数の定義/test.cs
@@ -0,0 +1,46 @@
+﻿/*【プログラム概要】
+ * 変数を定義して利用する。
+*/
+
+// 【プログラム】
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int foo;
+        foo = 100;
+        Debug.Log(foo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+/*【考察】
+ * 変数とは、
+ * データをしまっておく
+ * 箱みたいなものになる。
+ * 
+ * int(整数型)の
+ * foo(変数名)を定義、
+ * 100(データ)を代入し、
+ * 表示させている。
+ * 
+ * 変数の型には、
+ * 
+ * int    : 整数型
+ * float  : 浮動小数点型
+ * double : 倍精度浮動小数点型
+ * bool   : ブール型
+ * char   : 文字型
+ * string : 文字列型
+ * 
+ * などがある。
+ */

4b02245c10aaef461e148e35547cf14fdf657107
Ayu Iha <17oa03@gmail.com>
2017-07-26 00:18:14 +0900

    「C」の「基礎実習」にて、過去の学習内容を復習し、内容を更新をしました。

--- C/01_基礎実習/06_関数/05_円の面積計算/6-5_円の面積計算.c
+++ C/01_基礎実習/06_関数/05_円の面積計算/6-5_円の面積計算.c
@@ -9,67 +9,59 @@
 */
 #include <stdio.h>
 
-/* 入力を処理する関数 */
-int Input(void);
-/* 面積を計算する関数 */
-double Calculate(int radius);
-/* 出力を処理する関数 */
-void Output(int, double);
+/* プロトタイプ宣言 */
+int    Input(void);         /* 半径入力 */
+double Calculate(int);      /* 面積計算 */
+void   Output(int, double); /* 半径と面積の出力 */
 
 int main(void)
 {
 	/* 半径 */
 	int radius;
+
 	/* 面積 */
 	double area;
 
-	/* 実引数 : なし | 戻り値 : radius */
-	radius = Input();
-	/* 実引数 : radius | 戻り値 : area */
-	area = Calculate(radius);
-	/* 実引数 : radius, area | 戻り値 : なし */
-	Output(radius, area);
+	radius = Input(); /* 実引数は「無し」、戻り値は「radius」 */
+
+	area = Calculate(radius); /* 実引数は「radius」、戻り値は「area」 */
+
+	Output(radius, area); /* 実引数は「radius, area」、戻り値は「無し」 */
 
 	return 0;
 }
 
-/* 仮引数 : なし */
-int Input(void)
+/* サブルーチン */
+int Input(void) /* 仮引数は「無し」 */
 {
-
+	/* 半径 */
 	int radius;
 
 	printf("半径 ==> ");
 	scanf("%d", &radius);
 
-	/* 戻り値 : radius */
-	return radius;
+	return radius; /* 戻り値は「radius」 */
 }
 
-/* 仮引数 : radius */
-double Calculate(int radius)
+/* サブルーチン */
+double Calculate(int radius) /* 仮引数は「radius」 */
 {
-
+	/* 面積 */
 	double area;
 
 	/* 面積の計算 */
-	area = (double) radius * radius * 3.14;
+	area = (double)radius * radius * 3.14;
 
-	/* 戻り値 : area */
-	return area;
+	return area; /* 戻り値は「area」 */
 }
 
-/* 仮引数 : area */
-void Output(int radius, double area)
+/* サブルーチン */
+void Output(int radius, double area) /* 仮引数は「area」 */
 {
-
 	printf("半径 %d の円の面積は %.2f です。\n", radius, area);
 
-	/* 戻り値 : なし */
-	return;
+	return; /* 戻り値は「無し」 */
 }
-
-
 
 /*
 【実行結果1】
@@ -79,4 +71,24 @@ void Output(int radius, double area)
 【実行結果2】
 半径 ==> 10
 半径 10 の円の面積は 314.00 です。
+*/
+
+/*
+【考察】
+今回は、
+引数と戻り値のデータ型に注意しながら
+プログラムを記述する必要があった。
+
+戻り値が無い
+関数のプログラムに関して、
+「return;」の記述が無くても
+プログラムは動作するが、
+「制御を戻す」という意味で
+文法的には記述が必要になる。
+
+また、
+「return;」の記述が無い場合は、
+サブルーチンが動作したままに
+なってしまうという理由で、
+記述は必要になる。
 */

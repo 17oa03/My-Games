2ec8a1a70b1b27228e6de9095a4fded80ab8ed09
Ayu Iha <17oa03@gmail.com>
2017-08-09 20:22:25 +0900

    「Java」の「基礎実習」に、学習内容を反映しました。

---
+++ Java/01_基礎実習/00_基本構文/05_Scanner クラスの入力/Menseki.java
@@ -0,0 +1,34 @@
+// 【プログラム】
+package com.github.java.test;
+
+import java.util.Scanner; // import構文( import パッケージ名.クラス名; )
+
+public class Menseki {
+	public static void main(String[] args) {
+
+		int tate; // 縦の長さ
+		int yoko; // 横の長さ
+		int menseki; // 面積
+
+		Scanner scanner = new Scanner(System.in); // 「Scanner」クラスのインスタンスを「Scanner」変数に代入
+
+		System.out.println("縦の長さ ==> ");
+		tate = scanner.nextInt(); // 標準入力
+		System.out.println("横の長さ ==> ");
+		yoko = scanner.nextInt(); // 標準入力
+
+		menseki = tate * yoko; // 面積の計算
+
+		System.out.println("面積は " + menseki + "です。"); // 面積の出力
+
+	}
+}
+
+/*
+【実行結果】
+縦の長さ ==>
+30
+横の長さ ==>
+40
+面積は 1200です。
+*/

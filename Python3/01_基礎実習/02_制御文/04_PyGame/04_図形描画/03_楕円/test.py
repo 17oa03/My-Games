d16f432794ce90a851daf5ac4a673feea0970575
17oa03 <17oa03@gmail.com>
2017-08-13 01:24:29 +0900

    「python3」の「基礎実習」に、学習内容を反映しました。

--- Python3/01_基礎実習/02_制御文/04_PyGame/04_図形描画/03_楕円/test.py
+++ Python3/01_基礎実習/02_制御文/04_PyGame/04_図形描画/03_楕円/test.py
@@ -1,6 +1,6 @@
 """
 【Summary】
-円を描画させる。
+楕円を描画させる。
 
 """
 
@@ -27,13 +27,12 @@ def main(): # メイン関数
                 sys.exit() # プログラム終了
 
         SCREEN.fill((255, 255, 255)) # 画面を全て白で塗潰す
-
-        pygame.draw.circle(SCREEN, (255, 0, 0), (50, 50), 20) # 赤で塗潰した円
-        pygame.draw.circle(SCREEN, (255, 0, 0), (150, 50), 20, 10) # 太線の赤円
-        pygame.draw.circle(SCREEN, (0, 255, 0), (50, 150), 10) # 緑円
-        pygame.draw.circle(SCREEN, (0, 255, 0), (150, 150), 20) # 緑円
-        pygame.draw.circle(SCREEN, (0, 255, 0), (250, 150), 30) # 緑円
-
+        
+        pygame.draw.ellipse(SCREEN, (255, 0, 0), (50, 50, 140, 60)) # 赤楕円
+        pygame.draw.ellipse(SCREEN, (255, 0, 0), (250, 30, 90, 90)) # 赤楕円
+        pygame.draw.ellipse(SCREEN, (0, 255, 0), (50, 150, 110, 60), 5) # 緑楕円
+        pygame.draw.ellipse(SCREEN, (0, 255, 0), ((250, 130), (90, 90)), 20) # 緑楕円
+        
         pygame.display.update() # 描画内容を画面に反映
         FPSCLOCK.tick(3) # 1 秒間に 3 回ループする
 
@@ -43,14 +42,13 @@ if __name__ == '__main__':
 
 """
 【考察】
-circle メソッドで円を描画できる、
+ellipse メソッドで楕円を描画できる、
 
-circle(Surface, color, pos, radius, width = 0) -> Rect
+ellipse(Surface, color, Rect, width = 0) -> Rect
 
 surface : 描画対象となる画像(Surface オブジェクト)
 color : 色
-pos : 中心座標
-radius : 半径
+Rect : 楕円に外接する矩形の位置とサイズ
 width : 線の幅(省略すると塗潰しになる)
 
 """

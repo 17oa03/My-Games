449b18a5e87c12bda0c7d3df53064217632c26d5
17oa03 <17oa03@gmail.com>
2017-09-23 19:45:01 +0900

    「Java」の「基礎実習」に、学習内容を反映しました。

--- Java/01_基礎実習/05_試験0925/03_問題3/08_プログラム8/Employee.java
+++ Java/01_基礎実習/05_試験0925/03_問題3/08_プログラム8/Employee.java
@@ -1,7 +1,7 @@
 ﻿public class Employee {
 	private int no; // 社員番号
 	private String name; // 社員名
-	private int kyuyo; // 給与
+	private int kyuyo; // 基本給
 	private int zangyo; // 残業代
 
 	public Employee(int no, String name, int kyuyo) { // コンストラクタ
@@ -22,17 +22,21 @@
 		return name;
 	}
 
-	public int getKyuyo() { // 給与ゲッター
+	public int getKyuyo() { // 基本給ゲッター
 		return kyuyo;
 	}
 
-	public void dispKyuyo() { // 給与出力
-		System.out.println("給与:" + (getKyuyo() + zangyo) + "円");
+	public int getZangyo() { // 残業代ゲッター
+		return zangyo;
 	}
 
-	public void dispData() { // 社員情報出力
-		System.out.println("社員番号:" + getNo() +
-							" 社員名:" + getName() +
-							" 給与:" + (getKyuyo() + zangyo + "円"));
+	public void dispKyuyo() { // 給与額出力メソッド
+		System.out.println("給与額:" + (kyuyo + zangyo) + "円");
+	}
+
+	public void dispData() { // 社員情報出力メソッド
+		System.out.println("社員番号:" + no +
+							" 社員名:" + name +
+							" 給与額:" + (kyuyo + zangyo + "円"));
 	}
 }

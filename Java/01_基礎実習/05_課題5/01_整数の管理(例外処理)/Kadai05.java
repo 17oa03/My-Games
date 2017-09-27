150bab64c0c8f8669ef0be84136384c325c50947
17oa03 <17oa03@gmail.com>
2017-09-27 20:26:07 +0900

    「Java」の「基礎実習」に、学習内容を反映しました。

---
+++ Java/01_基礎実習/05_課題5/01_整数の管理(例外処理)/Kadai05.java
@@ -0,0 +1,48 @@
+package jkadai05;
+
+public class Kadai05 {
+	public static void main(String[] args) {
+		int seisu1;
+		int seisu2;
+
+		if (2 == args.length) {
+			try {
+				seisu1 = Integer.parseInt(args[0]);
+				seisu2 = Integer.parseInt(args[1]);
+				System.out.println(
+						seisu1 + " * " + seisu2 + " = " + (seisu1 * seisu2));
+			} catch (NumberFormatException e) {
+				System.out.println(
+						"コマンドライン引数は、整数を入力してください。");
+			}
+		} else
+			System.out.println(
+					"コマンドライン引数は、2個入力してください。");
+	}
+}
+
+/*
+【実行結果1】
+50 * 60 = 3000
+※コマンドライン引数「50, 60」
+
+【実行結果2】
+コマンドライン引数は、整数を入力してください。
+※コマンドライン引数「abc, 60」
+
+【実行結果3】
+コマンドライン引数は、整数を入力してください。
+※コマンドライン引数「50, abc」
+
+【実行結果4】
+コマンドライン引数は、2個入力してください。
+※コマンドライン引数「50」
+
+【実行結果5】
+コマンドライン引数は、2個入力してください。
+※コマンドライン引数なし
+
+【実行結果6】
+コマンドライン引数は、2個入力してください。
+※コマンドライン引数「50, 60, 70」
+*/

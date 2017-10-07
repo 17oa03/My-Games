fef52230c1ed3353820abdc9a8c140f4681b4e92
17oa03 <17oa03@gmail.com>
2017-10-07 19:51:23 +0900

    「Java」の「基礎実習」に、学習内容を反映しました。

---
+++ Java/01_基礎実習/06_課題6/01_ArrayListへの追加・変更・削除/別解法/Kadai0601_2.java
@@ -0,0 +1,35 @@
+package jkadai06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Kadai0601_2 {
+	public static void main(String[] args) {
+		String str = "C 言語,データベース,Fortran,JSP,COBOL,サーブレット";
+		System.out.println("変更前文字列 = " + str);
+
+		List<String> list = new ArrayList<>(Arrays.asList(str.split(",")));
+		list.add(1, "Java");
+		list.set(3, "HTML");
+		list.remove(5);
+
+		System.out.println("変更後文字列 = " + String.join(",", list));
+	}
+}
+
+/*
+【実行結果】
+変更前文字列 = C 言語,データベース,Fortran,JSP,COBOL,サーブレット
+変更後文字列 = C 言語,Java,データベース,HTML,JSP,サーブレット
+*/
+
+/*
+【Memo】
+asList() により「固定長のリスト」になる為、
+add() 実行時に例外が投げられる。
+
+固定長になったインスタンスを
+ArrayList のコンストラクタに渡し、
+「可変長のリスト」にすることで解決する。
+*/

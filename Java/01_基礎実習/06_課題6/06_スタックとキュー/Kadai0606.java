a6e8906dd92c06ee3322a2163d890b31ee22140a
17oa03 <17oa03@gmail.com>
2017-10-13 23:08:25 +0900

    「Java」の「基礎実習」に、学習内容を反映しました。

--- Java/01_基礎実習/06_課題6/06_スタックとキュー/Kadai0606.java
+++ Java/01_基礎実習/06_課題6/06_スタックとキュー/Kadai0606.java
@@ -1,19 +1,20 @@
 package jkadai06;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Kadai0606 {
 	public static void main(String[] args) {
 		Stack<String> stack = new Stack<>(); // スタック
-		LinkedList<String> queue = new LinkedList<>(); // キュー
+		Queue<String> queue = new LinkedList<>(); // キュー
 
 		stack.push("a"); // push(a)
 		stack.push("b"); // push(b)
-		queue.add(stack.pop()); // enq(pop())
-		queue.add("c"); // enq(c)
+		queue.offer(stack.pop()); // enq(pop())
+		queue.offer("c"); // enq(c)
 		stack.push("d"); // push(d)
-		stack.push(queue.remove()); // push(deq())
+		stack.push(queue.poll()); // push(deq())
 		System.out.println("x = " + stack.pop()); // x←pop()
 	}
 }

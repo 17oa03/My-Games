736f1300eb1cbd9c5271d2cc95a2d4d641d5c91e
17oa03 <17oa03@gmail.com>
2017-12-17 21:32:18 +0900

    「JSP」の「基礎学習」に学習内容を反映しました。

---
+++ JSP/01_基礎学習/07_JSP課題3/meibo.sql
@@ -0,0 +1,9 @@
+drop table meibo purge;
+create table meibo(id number(3), name varchar2(20));
+
+insert into meibo(id, name) values(30, '上田');
+insert into meibo(id, name) values(40, '小川');
+insert into meibo(id, name) values(20, '山田');
+insert into meibo(id, name) values(15, '吉田');
+insert into meibo(id, name) values(25, '小野');
+commit;

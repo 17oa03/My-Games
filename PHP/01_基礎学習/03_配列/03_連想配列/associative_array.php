5c232f1ff6ea54f2d78929441ae11cb939175d2a
17oa03 <17oa03@gmail.com>
2018-06-23 12:08:10 +0900

    「PHP」の「基礎実習」に、学習内容を反映しました。

---
+++ PHP/01_基礎学習/03_配列/03_連想配列/associative_array.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>hello</title>
+</head>
+<body>
+<!-- 連想配列の定義 -->
+  <?php
+    $a = array('dog' => 'いぬ', 'cat' => 'ねこ');
+    echo $a['dog'], $a['cat'];
+  ?>
+</body>
+</html>

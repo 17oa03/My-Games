3bf63f49e6c86886cd3d62f2a9f23510ec2b8ce6
17oa03 <17oa03@gmail.com>
2018-06-24 19:37:12 +0900

    「PHP」の「基礎実習」に、学習内容を反映しました。

---
+++ PHP/01_基礎学習/05_演算子/11_文字列演算子/string_operator.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>hello</title>
+</head>
+<body>
+<!-- 文字列演算子 -->
+  <?php
+    $a = "Hello " . "PHP" . "!";
+    echo $a;
+  ?>
+</body>
+</html>
+
+<!--
+【result】
+Hello PHP!
+-->
